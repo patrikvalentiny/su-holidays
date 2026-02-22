bd9c63d65cf98bf445104f5a97dd30c018d424ee
patrikvalentiny <patrikvalentiny@gmail.com>
2026-02-22 18:02:15 +0100

    add pdf download event

--- src/features/holiday-request/components/PDFDownloadButton.tsx
+++ src/features/holiday-request/components/PDFDownloadButton.tsx
@@ -8,6 +8,7 @@ interface PDFDownloadButtonProps {
 
 const PDFDownloadButton: React.FC<PDFDownloadButtonProps> = ({ data }) => (
     <PDFDownloadLink
+        data-umami-event="download_pdf"
         document={<HolidayRequestPDF data={data} />}
         fileName={`Holiday_Request_${data.employeeName.replace(/\s+/g, '_')}.pdf`}
         className="btn btn-primary flex-1 p-2"
